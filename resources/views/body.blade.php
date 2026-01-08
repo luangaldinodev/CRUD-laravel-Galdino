e2ad80293098cb0eeae34029f70158b2c9c9e5c0
luangaldinodev <luangaldinodev@gmail.com>
2026-01-07 22:59:15 -0300

    feat: adiciona o footer

--- resources/views/body.blade.php
+++ resources/views/body.blade.php
@@ -23,20 +23,52 @@
 
     <header class="bg-indigo-950 shadow-xl">
         <nav class="flex items-center justify-between px-6 py-6">
-            <a class="text-white text-2xl font-semibold text-shadow-md flex items-center gap-2" href=" {{ route('raiz') }} "><i
-                    class="fa-regular fa-user"></i>
+            <a class="text-white text-2xl font-semibold text-shadow-md flex items-center gap-2"
+                href=" {{ route('raiz') }} "><i class="fa-regular fa-user"></i>
                 <h1>CRUD do Galdino</h1>
             </a>
             <ul class="flex items-center justify-between gap-6">
                 <li><a class="text-white font-medium hover:underline" href=" {{ route('raiz') }} ">Home</a></li>
-                <li><a class="text-white font-medium hover:underline" href=" {{ route('pessoa.index') }} ">Ver Pessoas</a></li>
-                <li><a class="text-white font-medium hover:underline" href=" {{ route('pessoa.create') }} ">Resgistrar Pessoa</a></li>
-                <li><a class="text-white font-medium hover:underline" href=" {{ route('docs.index') }} ">Documentação</a></li>
+                <li><a class="text-white font-medium hover:underline" href=" {{ route('pessoa.index') }} ">Ver
+                        Pessoas</a></li>
+                <li><a class="text-white font-medium hover:underline" href=" {{ route('pessoa.create') }} ">Resgistrar
+                        Pessoa</a></li>
+                <li><a class="text-white font-medium hover:underline"
+                        href=" {{ route('docs.index') }} ">Documentação</a></li>
             </ul>
         </nav>
     </header>
 
     @yield('body')
+
+    <footer class="bg-indigo-950 shadow-xl">
+        <nav class="grid grid-cols-1 md:grid-cols-3 gap-8 mx-6 pt-12 pb-6 border-b border-indigo-400/50">
+            <a class="text-white text-2xl font-semibold text-shadow-md flex items-start gap-2"
+                href=" {{ route('raiz') }} "><i class="fa-regular fa-user"></i>
+                <h1>CRUD do Galdino</h1>
+            </a>
+            <ul class="flex flex-col items-start justify-between gap-6">
+                <h1 class="text-white text-2xl font-semibold text-shadow-md">Links Rápidos</h1>
+                <li><a class="text-white font-medium hover:underline" href=" {{ route('raiz') }} ">Home</a></li>
+                <li><a class="text-white font-medium hover:underline" href=" {{ route('pessoa.index') }} ">Ver
+                        Pessoas</a></li>
+                <li><a class="text-white font-medium hover:underline" href=" {{ route('pessoa.create') }} ">Resgistrar
+                        Pessoa</a></li>
+                <li><a class="text-white font-medium hover:underline"
+                        href=" {{ route('docs.index') }} ">Documentação</a></li>
+            </ul>
+
+            <ul class="flex flex-col items-start gap-6 text-white font-medium">
+                <h1 class="text-white text-2xl font-semibold text-shadow-md">Contato</h1>
+                <li><i class="fa-solid fa-envelope"></i> <a class="text-white font-medium underline" href="mailto: luangaldinodev@gmail.com">luangaldinodev@gmail.com</a></li>
+                <li><i class="fa-brands fa-instagram"></i> <a class="text-white font-medium underline" href="https://www.instagram.com/ogaldino.dev/" target="_blank" rel="noopener noreferrer">@ogaldino.dev</a></li>
+            </ul>
+        </nav>
+        <p class="text-center text-white text-lg text-shadow-md mb-2 mt-6">© <?php echo date('Y'); ?> Luan Galdino Goes. Todos os direitos reservados.</p>
+        <p class="text-center text-white text-md text-shadow-md pb-8">Feito com ❤ por Luan Galdino | G2A Digital - Marketing & Software</p>
+
+
+    </footer>
 
     <script>
         AOS.init();
